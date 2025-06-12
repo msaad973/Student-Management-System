617cdb660b8058691699495ce7356fc78072614b
msaad973 <muhammadsaadullah973@gmail.com>
2025-06-12 11:46:11 +0500

    assignment import store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './slices/userSlice';
+import assignmentReducer from './slices/assignmentSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
+    assignment: assignmentReducer
   },
 });
 
