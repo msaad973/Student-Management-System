4a5bb506bb713710616db77ae0f5b3408258e6a7
msaad973 <muhammadsaadullah973@gmail.com>
2025-06-26 11:35:59 +0500

    AuthGaurd Implemented in the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,48 @@ import EditUser from './pages/EditUser';
 import ViewUser from './pages/ViewUser';
 import ViewDetails from './pages/ViewDetails';
 
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import ResponsiveDrawer from './components/sideBar';
 import AssignmentPage from './pages/Assignment';
 import QuizPage from './pages/QuizPage';
 import StudentAssignments from './pages/StudentAssignment';
+import React from 'react';
+
+// AuthGuard component
+function AuthGuard({ children }) {
+  const location = useLocation();
+  const userToken = localStorage.getItem('userToken');
+
+  // Public pages that do not require authentication
+  const publicRoutes = ['/login', '/signup'];
+
+  // If not logged in and not on a public page, redirect to login
+  if (!userToken && !publicRoutes.includes(location.pathname)) {
+    return <Navigate to="/login" replace />;
+  }
+
+  // If logged in and trying to access login/signup, redirect to dashboard
+  if (userToken && publicRoutes.includes(location.pathname)) {
+    return <Navigate to="/" replace />;
+  }
+
+  // Otherwise, render children (protected or public)
+  return children;
+}
 
 function AppContent() {
   const location = useLocation();
-  
+
   // Define pages that should NOT show the sidebar
-  const noSidebarPages = ['/signup', '/login','/','/edituser'];
-  
+  const noSidebarPages = ['/signup', '/login', '/', '/edituser'];
+
   // Check if current path should show sidebar
   const showSidebar = !noSidebarPages.includes(location.pathname);
 
   return (
-    <>
+    <AuthGuard>
       {showSidebar ? (
         <ResponsiveDrawer>
           <Routes>
@@ -40,16 +63,16 @@ function AppContent() {
           </Routes>
         </ResponsiveDrawer>
       ) : (
-        <Routes> 
+        <Routes>
           <Route path="/" element={<ViewUser />} />
-          <Route path="/view-details" element={<View-details />} />
+          <Route path="/view-details" element={<ViewDetails />} />
           <Route path="/edituser" element={<EditUser />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="*" element={<div style={{ textAlign: 'center', marginTop: '50px' }}>404 Page Not Found</div>} />
         </Routes>
       )}
-    </>
+    </AuthGuard>
   );
 }
 
